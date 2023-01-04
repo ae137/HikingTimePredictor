89596ba4fa3233e30edeb454807b59284d313f40
Andreas Eberlein <28952639+ae137@users.noreply.github.com>
2023-01-04 22:25:08 +0100

    Improve dataset splitting and improve formatting (#15)

--- test_gpx_stats.py
+++ test_gpx_stats.py
@@ -1,28 +1,35 @@
 import unittest
 
-import numpy as np    # type: ignore
-import pandas as pd   # type: ignore
+import numpy as np
+import pandas as pd
 
-from gpxpy.gpx import GPXTrackPoint, GPXTrackSegment  # type: ignore
+from gpxpy.gpx import GPXTrackPoint, GPXTrackSegment
 
 from gpx_data_utils import (
     gpx_point_to_array,
     gpx_segment_to_array,
-    gpx_segment_from_array
+    gpx_segment_from_array,
 )
 from gpx_stats import convert_path_to_feature, smoothen_coordinates
 
 
 class TestConvertPathToArray(unittest.TestCase):
     def setUp(self):
-        self.segment_1 = GPXTrackSegment([GPXTrackPoint(longitude=.1, latitude=1, elevation=10),
-                                          GPXTrackPoint(longitude=.2, latitude=2, elevation=20),
-                                          GPXTrackPoint(longitude=.3, latitude=3, elevation=30)])
-        self.expected_path_1 = \
-            np.array([[0., 0., 0.],
-                      [1.0049875621, 0., 10],
-                      [2.00997512422, 0., 20],
-                      [0., 0., 0.]])
+        self.segment_1 = GPXTrackSegment(
+            [
+                GPXTrackPoint(longitude=0.1, latitude=1, elevation=10),
+                GPXTrackPoint(longitude=0.2, latitude=2, elevation=20),
+                GPXTrackPoint(longitude=0.3, latitude=3, elevation=30),
+            ]
+        )
+        self.expected_path_1 = np.array(
+            [
+                [0.0, 0.0, 0.0],
+                [1.0049875621, 0.0, 10],
+                [2.00997512422, 0.0, 20],
+                [0.0, 0.0, 0.0],
+            ]
+        )
 
     def test_segment_empty(self):
         with self.assertRaises(AssertionError):
@@ -39,13 +46,21 @@ class TestConvertPathToArray(unittest.TestCase):
 
 class TestSmoothenCoordinatesShort(unittest.TestCase):
     def setUp(self):
-        self.segment_1 = GPXTrackSegment([GPXTrackPoint(longitude=.1, latitude=1, elevation=10),
-                                          GPXTrackPoint(longitude=.2, latitude=2, elevation=20),
-                                          GPXTrackPoint(longitude=.3, latitude=3, elevation=30),
-                                          GPXTrackPoint(longitude=.5, latitude=-1, elevation=0)])
+        self.segment_1 = GPXTrackSegment(
+            [
+                GPXTrackPoint(longitude=0.1, latitude=1, elevation=10),
+                GPXTrackPoint(longitude=0.2, latitude=2, elevation=20),
+                GPXTrackPoint(longitude=0.3, latitude=3, elevation=30),
+                GPXTrackPoint(longitude=0.5, latitude=-1, elevation=0),
+            ]
+        )
 
-        self.expected_1 = GPXTrackSegment([GPXTrackPoint(longitude=.2, latitude=2, elevation=20),
-                                           GPXTrackPoint(longitude=1./3, latitude=4./3, elevation=50./3)])
+        self.expected_1 = GPXTrackSegment(
+            [
+                GPXTrackPoint(longitude=0.2, latitude=2, elevation=20),
+                GPXTrackPoint(longitude=1.0 / 3, latitude=4.0 / 3, elevation=50.0 / 3),
+            ]
+        )
 
     def test_segment_list_empty(self):
         smoothen_coordinates([])
@@ -61,8 +76,10 @@ class TestSmoothenCoordinatesShort(unittest.TestCase):
 
         self.assertEqual(len(input), 1)
         self.assertEqual(len(input[0].points), 1)
-        np.testing.assert_array_almost_equal(gpx_point_to_array(input[0].points[0]),
-                                             gpx_point_to_array(expected_result[0].points[0]))
+        np.testing.assert_array_almost_equal(
+            gpx_point_to_array(input[0].points[0]),
+            gpx_point_to_array(expected_result[0].points[0]),
+        )
 
     def test_segment_short_2(self):
         input = [GPXTrackSegment(self.segment_1.points)]
@@ -72,8 +89,10 @@ class TestSmoothenCoordinatesShort(unittest.TestCase):
 
         self.assertEqual(len(input), 1)
         self.assertEqual(len(input[0].points), len(expected_result[0].points))
-        np.testing.assert_array_almost_equal(gpx_point_to_array(input[0].points[0]),
-                                             gpx_point_to_array(expected_result[0].points[0]))
+        np.testing.assert_array_almost_equal(
+            gpx_point_to_array(input[0].points[0]),
+            gpx_point_to_array(expected_result[0].points[0]),
+        )
 
 
 class TestSmoothenCoordinatesLonger(unittest.TestCase):
@@ -88,25 +107,31 @@ class TestSmoothenCoordinatesLonger(unittest.TestCase):
         self.segments_as_arrays = [segment_1_as_array, segment_2_as_array]
 
     def test_window_size_3(self):
-        expected_as_df = [pd.DataFrame(self.segments_as_arrays[i]).rolling(3).mean()[2:] for i in
-                          range(len(self.segments_as_arrays))]
+        expected_as_df = [
+            pd.DataFrame(self.segments_as_arrays[i]).rolling(3).mean()[2:]
+            for i in range(len(self.segments_as_arrays))
+        ]
 
         smoothen_coordinates(self.segments, window_size=3)
 
         for i in range(len(self.segments)):
-            np.testing.assert_array_almost_equal(expected_as_df[i].values,
-                                                 gpx_segment_to_array(self.segments[i]))
+            np.testing.assert_array_almost_equal(
+                expected_as_df[i].values, gpx_segment_to_array(self.segments[i])
+            )
 
     def test_window_size_5(self):
-        expected_as_df = [pd.DataFrame(self.segments_as_arrays[i]).rolling(5).mean()[4:] for i in
-                          range(len(self.segments_as_arrays))]
+        expected_as_df = [
+            pd.DataFrame(self.segments_as_arrays[i]).rolling(5).mean()[4:]
+            for i in range(len(self.segments_as_arrays))
+        ]
 
         smoothen_coordinates(self.segments, window_size=5)
 
         for i in range(len(self.segments)):
-            np.testing.assert_array_almost_equal(expected_as_df[i].values,
-                                                 gpx_segment_to_array(self.segments[i]))
+            np.testing.assert_array_almost_equal(
+                expected_as_df[i].values, gpx_segment_to_array(self.segments[i])
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- utils.py
+++ utils.py
@@ -1,9 +1,16 @@
-import numpy as np               # type: ignore
-import pandas as pd              # type: ignore
-import matplotlib.pyplot as plt  # type: ignore
+from typing import Union
+
+import hashlib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from pathlib import Path
 
 
-def compute_standard_walking_time(length_m: float, elevation_gain_m: float, elevation_loss_m: float) -> float:
+def compute_standard_walking_time(
+    length_m: float, elevation_gain_m: float, elevation_loss_m: float
+) -> float:
     """Estimate the hiking time in seconds
 
     Parameters
@@ -14,15 +21,18 @@ def compute_standard_walking_time(length_m: float, elevation_gain_m: float, elev
 
     :return: Walking time (= moving time) for the hike in seconds
     """
-    walking_time_length_s = 0.9 * length_m          # assumed speed: 4 km/h
-    walking_time_up_s = elevation_gain_m * 12       # assumed speed: +300 m/h
-    walking_time_down_s = elevation_loss_m * 7.2    # assumed speed: -500 m/h
+    walking_time_length_s = 0.9 * length_m  # assumed speed: 4 km/h
+    walking_time_up_s = elevation_gain_m * 12  # assumed speed: +300 m/h
+    walking_time_down_s = elevation_loss_m * 7.2  # assumed speed: -500 m/h
     walking_time_up_down_s = walking_time_up_s + walking_time_down_s
-    return max(walking_time_length_s, walking_time_up_down_s) + \
-        0.5 * min(walking_time_length_s, walking_time_up_down_s)
+    return max(walking_time_length_s, walking_time_up_down_s) + 0.5 * min(
+        walking_time_length_s, walking_time_up_down_s
+    )
 
 
-def scatter_plot(ground_truth: np.array, predictions: np.array, label_text: str) -> None:
+def scatter_plot(
+    ground_truth: np.array, predictions: np.array, label_text: str
+) -> None:
     """
     Plot scatter plot of predictions vs. ground truth values.
 
@@ -30,11 +40,11 @@ def scatter_plot(ground_truth: np.array, predictions: np.array, label_text: str)
     :param: predictions:    Array containing predicted values
     :param: label_text:     Label text for plot
     """
-    plt.scatter(ground_truth, predictions, label='{} scatter plot'.format(label_text))
-    max_val = max([max(ground_truth), max(predictions)])
-    plt.xlabel('True Values [{}]'.format(label_text))
-    plt.ylabel('Predictions [{}]'.format(label_text))
-    plt.axis('square')
+    plt.scatter(ground_truth, predictions, label="{} scatter plot".format(label_text))
+    max_val: float = max([max(ground_truth), max(predictions)])
+    plt.xlabel("True Values [{}]".format(label_text))
+    plt.ylabel("Predictions [{}]".format(label_text))
+    plt.axis("square")
     plt.legend()
     plt.xlim([0, max_val])
     plt.ylim([0, max_val])
@@ -42,7 +52,9 @@ def scatter_plot(ground_truth: np.array, predictions: np.array, label_text: str)
     plt.show()
 
 
-def plot_error_hist(ground_truth: np.array, predictions: np.array, label_text: str) -> None:
+def plot_error_hist(
+    ground_truth: np.array, predictions: np.array, label_text: str
+) -> None:
     """
     Plot historgram of errors.
 
@@ -64,30 +76,56 @@ def plot_history(history, mse_column_name: str, mae_column_name: str):
     :param: history: Keras History object
     """
     hist = pd.DataFrame(history.history)
-    hist['epoch'] = history.epoch
+    hist["epoch"] = history.epoch
 
     plt.figure()
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Absolute Error [MovingTime]')
-    plt.plot(hist['epoch'], hist[mae_column_name], label='Train Error')
-    plt.plot(hist['epoch'], hist['val_' + mae_column_name], label='Val Error')
-    plt.yscale('log')
+    plt.xlabel("Epoch")
+    plt.ylabel("Mean Absolute Error [MovingTime]")
+    plt.plot(hist["epoch"], hist[mae_column_name], label="Train Error")
+    plt.plot(hist["epoch"], hist["val_" + mae_column_name], label="Val Error")
+    plt.yscale("log")
     plt.legend()
 
     plt.figure()
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Squared Error [$MovingTime^2$]')
-    plt.plot(hist['epoch'], hist[mse_column_name], label='Train Error')
-    plt.plot(hist['epoch'], hist['val_' + mse_column_name], label='Val Error')
-    plt.yscale('log')
+    plt.xlabel("Epoch")
+    plt.ylabel("Mean Squared Error [$MovingTime^2$]")
+    plt.plot(hist["epoch"], hist[mse_column_name], label="Train Error")
+    plt.plot(hist["epoch"], hist["val_" + mse_column_name], label="Val Error")
+    plt.yscale("log")
     plt.legend()
     plt.show()
 
     if "lr" in hist.columns:
         plt.figure()
-        plt.xlabel('Epoch')
-        plt.ylabel('Learning rate')
-        plt.plot(hist['epoch'], hist['lr'], label="Learning rate")
-        plt.yscale('log')
+        plt.xlabel("Epoch")
+        plt.ylabel("Learning rate")
+        plt.plot(hist["epoch"], hist["lr"], label="Learning rate")
+        plt.yscale("log")
         plt.legend()
         plt.show()
+
+
+def get_pseudo_probability_for_path(
+    path: Union[str, Path], max_int: int = 4096
+) -> float:
+    """Create pseudo probability for dataset split assignment of files.
+
+    Parameters
+    ----------
+    path
+        Path to file
+    max_int
+        Maximal integer number that should be allowed for modulo operations
+
+    Returns
+    -------
+    Pseudo-probability score that is useful for unique and reproducible assignment of files to dataset split
+    """
+    if isinstance(path, Path):
+        path = str(Path)
+
+    encoded_path = str.encode(path)
+
+    hash_as_int = int(hashlib.sha256(encoded_path).hexdigest(), 16)
+
+    return (hash_as_int % max_int) / (max_int - 1)
